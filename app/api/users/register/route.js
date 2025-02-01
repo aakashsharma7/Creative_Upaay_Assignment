6a218350ff90567843ffea17445b1b7bb2b0daed
aakashsharma7 <aakashsharma.ghd@gmail.com>
2025-02-01 20:22:21 +0530

    completed

--- app/api/users/register/route.js
+++ app/api/users/register/route.js
@@ -1,3 +1,44 @@
+// import Connection from '@/database/config';
+// import User from '@/models/user';
+// import bcryptjs from 'bcryptjs';
+// import { NextRequest, NextResponse } from 'next/server';
+
+// Connection();
+
+// export const POST = async (NextRequest) => {
+//     try {
+//         const body = await NextRequest.json();
+//         const { name, username, password } = body;
+
+//         if (!name || !username || !password) {
+//             return new Response("Name, Username and Password is required", { status: 401 });
+//         }
+
+//         const user = await User.findOne({ username });
+//         if (user) {
+//             return new Response("Username already exist", { status: 400 });
+//         }
+
+//         const salt = await bcryptjs.genSalt(12);
+//         const hashedPassword = await bcryptjs.hash(password, salt);
+
+//         const newUser = new User({
+//             name,
+//             username,
+//             password: hashedPassword,
+//         })
+
+//         await newUser.save();
+        
+//         return new Response("User saved successfully", { status: 200 });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+
+
+
 import Connection from '@/database/config';
 import User from '@/models/user';
 import bcryptjs from 'bcryptjs';
@@ -11,12 +52,12 @@ export const POST = async (NextRequest) => {
         const { name, username, password } = body;
 
         if (!name || !username || !password) {
-            return new Response("Name, Username and Password is required", { status: 401 });
+            return NextResponse.json({ message: "Name, Username, and Password are required" }, { status: 400 });
         }
 
         const user = await User.findOne({ username });
         if (user) {
-            return new Response("Username already exist", { status: 400 });
+            return NextResponse.json({ message: "Username already exists" }, { status: 400 });
         }
 
         const salt = await bcryptjs.genSalt(12);
@@ -26,16 +67,13 @@ export const POST = async (NextRequest) => {
             name,
             username,
             password: hashedPassword,
-        })
+        });
 
         await newUser.save();
         
-        return new Response("User saved successfully", { status: 200 });
+        return NextResponse.json({ message: "User saved successfully" }, { status: 200 });
     } catch (error) {
-        console.log(error);
+        console.error(error);
+        return NextResponse.json({ message: "An error occurred" }, { status: 500 });
     }
-}
-
-
-
-
+};
